1bacdfa3e74238470e64207b0dbd4a5d177aacd5
mwon0326 <37972833+mwon0326@users.noreply.github.com>
2020-12-05 17:31:16 +0900

    raven
    과제1

---
+++ Common/lua-5.1.3/include/OpenLuaStates.h
@@ -0,0 +1,13 @@
+#ifndef OPEN_LUA_STATES
+#define OPEN_LUA_STATES
+
+extern "C"
+{
+  #include <lua-5.1.3/include/lua.h>
+  #include <lua-5.1.3/include/lualib.h>
+  #include <lua-5.1.3/include/lauxlib.h>
+}
+
+
+
+#endif
